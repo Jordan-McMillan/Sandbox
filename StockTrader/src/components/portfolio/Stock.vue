d1d4e83fa39e9632ce4818f73be01010a6bd7842
Jordan McMillan <jmcmillan@evestment.com>
2018-06-25 16:01:44 -0400

    stock app tutorial

---
+++ StockTrader/src/components/portfolio/Stock.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    {{ stock.name }}
+                    <small>Price: {{ stock.price }} | Quantity: {{ stock.quantity }}</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input 
+                        type="number"
+                        class="form-control"
+                        placeholder="Quantity" 
+                        v-model.number="quantity"
+                        @keyup.enter="sellStock"
+                        :class="{danger: insufficientQuantity}" />
+                    <div>
+                        Total: {{ totalCost | currency }}
+                    </div>
+                </div>
+                <div class="pull-right">
+                    <button 
+                        class="btn btn-info" 
+                        @click="sellStock"
+                        :disabled="quantity <= 0 || !Number.isInteger(quantity) || insufficientQuantity"
+                        >{{ insufficientQuantity ? '---' : 'SELL' }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import {mapActions} from 'vuex';
+
+    export default {
+        data() {
+            return {
+                quantity: 0
+            };            
+        },
+        props: {
+            stock: Object            
+        },
+        computed: {
+            insufficientQuantity() {
+                return this.quantity > this.stock.quantity;
+            },
+            totalCost() {
+                return this.quantity * this.stock.price;
+            }
+        },
+        methods: {
+            ...mapActions({
+                placeSellOrder: 'sellStock'
+            }),
+            sellStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                this.placeSellOrder(order);
+                this.quantity = 0;
+            }
+        }
+    }
+    
+</script>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
---
+++ StockTrader/src/components/stocks/Stock.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    {{ stock.name }}
+                    <small>Price: {{ stock.price }}</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input 
+                        type="number"
+                        class="form-control"
+                        placeholder="Quantity" 
+                        v-model.number="quantity"
+                        @keyup.enter="buyStock"
+                        :class="{danger: insufficientFunds}" />
+                    <div>
+                        Total: {{ totalCost | currency }}
+                    </div>
+                </div>
+                <div class="pull-right">
+                    <button 
+                        class="btn btn-success" 
+                        @click="buyStock"
+                        :disabled="quantity <= 0 || !Number.isInteger(quantity) || insufficientFunds" >
+                        {{ insufficientFunds ? '---' : 'BUY' }}
+                        </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                quantity: 0
+            };            
+        },
+        props: {
+            stock: Object            
+        },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return (this.quantity * this.stock.price) > this.funds;
+            },
+            totalCost() {
+                return this.quantity * this.stock.price;
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                this.$store.dispatch('buyStock', order);
+                this.quantity = 0;
+            }
+        }
+    }
+    
+</script>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
