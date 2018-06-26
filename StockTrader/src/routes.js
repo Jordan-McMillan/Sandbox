d09c74dd403ebd0f6b12f325b05713650ef59874
Jordan McMillan <jmcmillan@evestment.com>
2018-06-26 10:04:50 -0400

    dynamic loading

--- StockTrader/src/routes.js
+++ StockTrader/src/routes.js
@@ -1,6 +1,21 @@
-import Home from './components/Home.vue';
-import Portfolio from './components/portfolio/Portfolio.vue';
-import Stocks from './components/stocks/Stocks.vue';
+const Home = resolve => {
+	require.ensure(['./components/Home.vue'], () => {
+		resolve(require('./components/Home.vue'));
+	});
+};
+
+const Portfolio = resolve => {
+	require.ensure(['./components/portfolio/Portfolio.vue'], () => {
+		resolve(require('./components/portfolio/Portfolio.vue'));
+	});
+};
+
+const Stocks = resolve => {
+	require.ensure(['./components/stocks/Stocks.vue'], () => {
+		resolve(require('./components/stocks/Stocks.vue'));
+	});
+};
+
 
 export const routes = [
     { path: '/', component: Home },
